f345ec20aff2860fcaa9cf5e340f38e8ce97b674
eugeniusms <eugeniusms@gmail.com>
2024-06-19 18:10:35 +0700

    refactor: remove unused codes

--- app/user_history/views.py
+++ app/user_history/views.py
@@ -2,8 +2,6 @@ from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 from config.response import SuccessResponse, FailedResponse
-from app.recommendation.models import Disease
-from app.recommendation.serializers import DiseaseSerializer
 from app.user_history.models import UserHistory
 from app.user_history.serializers import UserHistorySerializer
 
